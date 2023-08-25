fa1e1156dc3899aeb46a22e6965d829a0a11a143
Maro Umuomo <oghenemaro.umuomo@outlook.com>
2023-08-25 15:52:13 +0200

    Expand features for selecting coffee type and showing available ingredients in Coffee Machine

--- Main.kt
+++ Main.kt
@@ -1,15 +1,6 @@
 package machine
 
-const val WATER = 200
-const val MILK = 50
-const val COFFEE_BEANS = 15
-
-fun getWater(cups: Int) = cups * WATER
-fun getMilk(cups: Int) = cups * MILK
-fun getBeans(cups: Int) = cups * COFFEE_BEANS
-fun getCupsWater(waterLeft: Int) = waterLeft / WATER
-fun getCupsMilk(milkLeft: Int) = milkLeft / MILK
-fun getCupsBeans(beansLeft: Int) = beansLeft / COFFEE_BEANS
+import kotlin.system.exitProcess
 
 data class Espresso(
     val water: Int = 250,
@@ -47,6 +38,7 @@ val COFFEEMACHINE = CoffeeMachine()
 
 fun printStatus()
 {
+    println()
     println(
         """
         The coffee machine has:
@@ -57,38 +49,70 @@ fun printStatus()
         ${'$'}${COFFEEMACHINE.cash} of money 
         """.trimIndent()
     )
+    println()
 }
 
-fun calculateBalance(coffeeType: Int)
+fun calculateBalance(coffeeType: String)
 {
     when (coffeeType) {
-        1 -> {
+        "1" -> {
             COFFEEMACHINE.water -= ESPRESSO.water
             COFFEEMACHINE.milk -= ESPRESSO.milk
             COFFEEMACHINE.beans -= ESPRESSO.beans
             COFFEEMACHINE.cash += ESPRESSO.price
             --COFFEEMACHINE.cups
         }
-        2 -> {
+        "2" -> {
             COFFEEMACHINE.water -= LATTE.water
             COFFEEMACHINE.milk -= LATTE.milk
             COFFEEMACHINE.beans -= LATTE.beans
             COFFEEMACHINE.cash += LATTE.price
             --COFFEEMACHINE.cups
         }
-        3 -> {
+        "3" -> {
             COFFEEMACHINE.water -= CAPPUCCINO.water
             COFFEEMACHINE.milk -= CAPPUCCINO.milk
             COFFEEMACHINE.beans -= CAPPUCCINO.beans
             COFFEEMACHINE.cash += CAPPUCCINO.price
             --COFFEEMACHINE.cups
         }
+        else -> return
     }
+}
+
+fun espressoRequest(): IntArray {
+    val ingredients = IntArray(3)
+    when {
+        COFFEEMACHINE.water < ESPRESSO.water -> ++ingredients[0]
+        COFFEEMACHINE.milk < ESPRESSO.milk -> ++ingredients[1]
+        COFFEEMACHINE.beans < ESPRESSO.beans -> ++ingredients[2]
+    }
+    return ingredients
+}
+
+fun latteRequest(): IntArray {
+    val ingredients = IntArray(3)
+    when {
+        COFFEEMACHINE.water < LATTE.water -> ++ingredients[0]
+        COFFEEMACHINE.milk < LATTE.milk -> ++ingredients[1]
+        COFFEEMACHINE.beans < LATTE.beans -> ++ingredients[2]
+    }
+    return ingredients
+}
+
+fun cappuccinoRequest(): IntArray {
+    val ingredients = IntArray(3)
+    when {
+        COFFEEMACHINE.water < CAPPUCCINO.water -> ++ingredients[0]
+        COFFEEMACHINE.milk < CAPPUCCINO.milk -> ++ingredients[1]
+        COFFEEMACHINE.beans < CAPPUCCINO.beans -> ++ingredients[2]
+    }
+    return ingredients
 }
 
 fun fillMachine()
 {
-    println("Write how many ml of water you want to add: ")
+    println("\nWrite how many ml of water you want to add: ")
     var userInput = readln().toInt()
     COFFEEMACHINE.water += userInput
     println("Write how many ml of milk you want to add: ")
@@ -97,39 +121,76 @@ fun fillMachine()
     println("Write how many grams of coffee beans you want to add: ")
     userInput = readln().toInt()
     COFFEEMACHINE.beans += userInput
-    println("Write how many disposable cups you want to add:")
+    println("Write how many disposable cups you want to add: ")
     userInput = readln().toInt()
     COFFEEMACHINE.cups += userInput
     println()
-    printStatus()
+    //printStatus()
 }
 
 fun buyCoffee()
 {
-    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
-    val choice = readln().toInt()
-    calculateBalance(choice)
-    println()
-    printStatus()
+    println("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+    val choice = readln()
+
+    if (choice == "back") {
+        println()
+        return
+    }
+
+    val availability = requestPossible(choice)
+
+    if (availability.sum() == 0) {
+        calculateBalance(choice)
+        println("I have enough resources, making you a coffee!\n")
+    }else
+    {
+        val outputPrint = availability.joinToString("")
+        when(outputPrint) {
+            "001" -> println("Sorry, not enough beans!\n")
+            "010" -> println("Sorry, not enough milk!\n")
+            "011" -> println("Sorry, not enough milk and beans!\n")
+            "100" -> println("Sorry, not enough water!\n")
+            "101" -> println("Sorry, not enough water and beans!\n")
+            "110" -> println("Sorry, not enough water and milk!\n")
+            "111" -> println("Sorry, not enough resources!\n")
+            else -> println("Sorry, not enough resources!\n")
+        }
+    }
+    //println()
+    //printStatus()
+}
+
+fun requestPossible(choice: String): IntArray{
+    var ingredients = IntArray(3)
+    when (choice.toInt()) {
+        1 -> ingredients = espressoRequest()
+        2 -> ingredients = latteRequest()
+        3 -> ingredients = cappuccinoRequest()
+    }
+    return ingredients
 }
 
 fun takeMoney()
 {
-    println("I gave you ${'$'}${COFFEEMACHINE.cash}")
+    println("\nI gave you ${'$'}${COFFEEMACHINE.cash}")
     COFFEEMACHINE.cash = 0
     println()
-    printStatus()
+    //printStatus()
 }
 
 fun main() {
 
-    printStatus()
-    println("\nWrite action (buy, fill, take): ")
-    val action = readln()
-
-    when (action) {
-        "buy" -> buyCoffee()
-        "fill" -> fillMachine()
-        "take" -> takeMoney()
+    while (true)
+    {
+        println("Write action (buy, fill, take, remaining, exit): ")
+        val action = readln()
+        when (action) {
+            "buy" -> buyCoffee()
+            "fill" -> fillMachine()
+            "take" -> takeMoney()
+            "remaining" -> printStatus()
+            "exit" -> break
+        }
     }
 }
